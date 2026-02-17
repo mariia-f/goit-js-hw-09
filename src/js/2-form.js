ec42848bd317d44241a74eb7dca742e7153fb4f7
mariia <maryfurman101107@gmail.com>
2026-02-17 22:16:03 +0200

    fixed

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,36 +6,44 @@ const formData = {
 const feedbackForm = document.querySelector(".feedback-form");
 
 const savedData = localStorage.getItem("feedback-form-state");
+
 if (savedData) {
   const parsedData = JSON.parse(savedData);
-  formData.email = parsedData.email ;
-  formData.message = parsedData.message;
-  
+
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+
   feedbackForm.elements.email.value = formData.email;
   feedbackForm.elements.message.value = formData.message;
 }
 
 feedbackForm.addEventListener("input", (event) => {
-      const { name, value } = event.target;
- formData[name] = value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData))
-})
-    
+  const { name, value } = event.target;
+
+  formData[name] = value;
+
+  localStorage.setItem(
+    "feedback-form-state",
+    JSON.stringify(formData)
+  );
+});
 
 feedbackForm.addEventListener("submit", (event) => {
-    
-    event.preventDefault();
-    const form = event.target;
-    const email = form.elements.email.value.trim();
-    const message = form.elements.message.value.trim();
-    if (email === "" || message === "") {
-        alert('Fill please all fields');
-    }
-    else {
-   
-        console.log(formData);
-        form.elements.email.value = "";
-        form.elements.message.value = "";
-    }
-    form.reset();
-})
+  event.preventDefault();
+
+  const email = formData.email.trim();
+  const message = formData.message.trim();
+
+  if (email === "" || message === "") {
+    alert("Fill please all fields");
+    return; 
+  }
+
+ 
+  console.log(formData);
+  feedbackForm.reset();
+
+  formData.email = '';
+  formData.message = '';
+  localStorage.removeItem("feedback-form-state");
+});
